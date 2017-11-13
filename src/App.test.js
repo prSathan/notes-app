10fde756feb0e6bee3997baa58fd33e1f54cdaa4
Prabu Sathan <prabu.sathan@gmail.com>
2017-11-13 15:31:05 +0000

    Tests now use enzyme, test that app component exists

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+configure({ adapter: new Adapter() });
+
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
