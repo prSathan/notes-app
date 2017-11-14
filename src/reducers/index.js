2734fa2d7017fbf838f234d1baac769be853c5db
Prabu Sathan <prabu.sathan@gmail.com>
2017-11-14 14:36:41 +0000

    Reducer can now add a note to state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,27 @@
-export const initialState = {};
+import types from '../constants/';
 
-export const reducer = (state = initialState, action) => state;
+export const initialState = {
+  notes: [],
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case types.SUBMIT_NOTE:
+      return {
+        ...state,
+        notes: [
+          ...state.notes,
+          {
+            id: action.id,
+            text: action.text,
+          },
+        ],
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
--- src/reducers/test.js
+++ src/reducers/test.js
@@ -1,7 +1,31 @@
+import types from '../constants/';
 import { reducer, initialState } from '.';
 
 describe('Reducer', () => {
+  const noteText = 'A note';
+
   it('Should return the initial state when no action passed', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  describe('Submit note', () => {
+    it('Should return the correct state', () => {
+      const action = {
+        type: types.SUBMIT_NOTE,
+        id: 1,
+        text: noteText,
+      };
+
+      const expectedState = {
+        notes: [
+          {
+            id: 1,
+            text: noteText,
+          },
+        ],
+      };
+
+      expect(reducer(undefined, action)).toEqual(expectedState);
+    });
   });
 });
